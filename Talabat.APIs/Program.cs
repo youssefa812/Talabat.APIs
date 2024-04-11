3c0859d510a5f26ae9c5f5ac1a5b70ae69c92748
Youssef <Youssef@DESKTOP-7EUM97G>
2024-04-11 23:20:16 +0200

    Update Database

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -6,7 +6,7 @@ namespace Talabat.APIs
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var webApplicationBuilder = WebApplication.CreateBuilder(args);
 			#region Configure Services
@@ -28,6 +28,20 @@ namespace Talabat.APIs
 			#endregion
 
 			var app = webApplicationBuilder.Build();
+			//Ask CLR for creating object from DBContext explicitly
+			using var scope = app.Services.CreateScope();
+			var services = scope.ServiceProvider;
+			var _dbContext = services.GetRequiredService<StoreContext>();
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+			try
+			{
+				await _dbContext.Database.MigrateAsync();
+			}
+			catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(ex, "An error has been occured during applying the migration");
+			}
 
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
