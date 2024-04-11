c23631e7a2fe4dfe2fe86b73e3954bbf4d67cf6a
Youssef <Youssef@DESKTOP-7EUM97G>
2024-04-11 23:01:39 +0200

    DbContext, ProductConfigurations, Migrations

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Talabat.Repository.Data;
+
 namespace Talabat.APIs
 {
 	public class Program
@@ -6,13 +9,23 @@ namespace Talabat.APIs
 		public static void Main(string[] args)
 		{
 			var webApplicationBuilder = WebApplication.CreateBuilder(args);
+			#region Configure Services
 
 			// Add services to the container.
 
 			webApplicationBuilder.Services.AddControllers();
+			// Register Required Web APIs Services to The DI Container
+
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			webApplicationBuilder.Services.AddEndpointsApiExplorer();
 			webApplicationBuilder.Services.AddSwaggerGen();
+
+			webApplicationBuilder.Services.AddDbContext<StoreContext>(options =>
+			{
+				options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
+			});
+
+			#endregion
 
 			var app = webApplicationBuilder.Build();
 
